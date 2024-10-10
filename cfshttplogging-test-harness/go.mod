2fb2fcef572028de2e025c2d2f255c3eee8a0ace
Connor <connor.sadler@gmail.com>
2024-10-10 17:53:17 +0100

    stuff 10102024

--- cfshttplogging-test-harness/go.mod
+++ cfshttplogging-test-harness/go.mod
@@ -2,4 +2,4 @@ module cfshttplogging-test-harness
 
 go 1.22.4
 
-require github.com/connorsadler/go-utils/cfshttplogging v0.0.0-20241010162005-3397a9ec44d6 // indirect
+require github.com/connorsadler/go-utils/cfshttplogging v0.0.0-20241010162721-ecfddd7a1b54 // indirect
--- cfshttplogging/cfshttplogging_test.go
+++ cfshttplogging/cfshttplogging_test.go
@@ -1,6 +1,9 @@
 package cfshttplogging
 
 import (
+	"io"
+	"log"
+	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -11,4 +14,24 @@ func TestSampleFuncFromConnor(t *testing.T) {
 	actualResult := SampleFuncFromConnor()
 
 	assert.Regexp(t, "This is from cfshttplogging.go at.*", actualResult)
+}
+
+func TestInstallLoggingRoundTripper(t *testing.T) {
+
+	c := http.DefaultClient
+	InstallLoggingRoundTripper(c)
+
+	resp, err := http.Get("https://api.sampleapis.com/beers/ale/1")
+	if err != nil {
+		log.Fatalf("Error: %v", err)
+	}
+
+	b, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Printf("Response body: %v", string(b))
+
+	assert.Equal(t, "TODO", "NOTDONEYET")
 }
